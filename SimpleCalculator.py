ddf14a4ed7e9edfe0badadf23be7aefa17c8ab2a
nawid88 <kariminawid88@gmail.com>
2024-09-19 14:31:19 -0400

    Simple Calculator Created

---
+++ SimpleCalculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the first number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1*num2
+division = num1/num2
+
+print(f"{num1} + {num2} = {addition}")
+print(f"{num1} - {num2} = {subtraction}")
+print(f"{num1} * {num2} = {multiplication}")
+print(f"{num1} / {num2} = {division}")
+
+
+
+
+
+
+
