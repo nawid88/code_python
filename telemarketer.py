c2c3a1641e9db98ac234015ee4b5de0f0a91a2c7
nawid88 <kariminawid88@gmail.com>
2024-09-21 23:08:14 -0400

    Telemarketer

---
+++ telemarketer.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter digit: "))
+num2 = int(input("Enter digit: "))
+num3 = int(input("Enter digit: "))
+num4 = int(input("Enter digit: "))
+
+if (num1 == 8  or num1 == 9) and (num2 == num3) and (num4 == 8  or num4 == 9):
+    print("Ignore")
+else:
+    print("Answer")
+
+
