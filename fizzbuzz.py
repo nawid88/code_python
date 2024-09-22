d6038364f4fd11647b0d08f327638a94cc571513
nawid88 <kariminawid88@gmail.com>
2024-09-21 23:04:17 -0400

    FizzBUzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+for num in list(range(1, 51)):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    else:
+        print(num)
+
+
