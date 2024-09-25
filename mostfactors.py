bee60f6aae9cfc4a4cad420433c3686f565f9a22
nawid88 <kariminawid88@gmail.com>
2024-09-25 13:52:25 -0400

    Most Factors

--- mostfactors.py
+++ mostfactors.py
@@ -1,5 +1,3 @@
-#From 1 to N, where N is an integer greater one. We will output the number between [1,N] that has the most factors. 
-#Example: From numbers 1 to 5. → The number 4 has the most factors.
 
 num = int(input("Enter a number: "))
 
